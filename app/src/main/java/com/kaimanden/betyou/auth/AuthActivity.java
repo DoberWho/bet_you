329b677388ddba3c032e78a3380361c2debe785e
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2020-11-30 21:48:39 +0100

    FIX

--- app/src/main/java/com/kaimanden/betyou/auth/AuthActivity.java
+++ app/src/main/java/com/kaimanden/betyou/auth/AuthActivity.java
@@ -14,7 +14,7 @@ public class AuthActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.act_login);
+        setContentView(R.layout.act_auth);
 
         mTextView = (TextView) findViewById(R.id.text);
 
