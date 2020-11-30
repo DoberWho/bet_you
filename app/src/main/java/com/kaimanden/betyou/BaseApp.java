a520e9cce01c7ffb265b4335f54f9f1dd9215914
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2020-11-30 21:43:32 +0100

    ADD: Lib => Hawk

---
+++ app/src/main/java/com/kaimanden/betyou/BaseApp.java
@@ -0,0 +1,25 @@
+package com.kaimanden.betyou;
+
+import android.app.Application;
+import android.content.res.Configuration;
+
+import com.orhanobut.hawk.Hawk;
+
+public class BaseApp extends Application {
+
+	@Override
+	public void onCreate() {
+	    super.onCreate();
+		Hawk.init(this).build();
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+	    super.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	public void onLowMemory() {
+	    super.onLowMemory();
+	}
+}
