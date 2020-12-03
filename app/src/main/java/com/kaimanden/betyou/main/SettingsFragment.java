57ec0dabb3ed4ae9de1268453214d8ceda3a5c3e
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2020-12-03 17:45:52 +0100

    ADD: ChangePass

--- app/src/main/java/com/kaimanden/betyou/main/SettingsFragment.java
+++ app/src/main/java/com/kaimanden/betyou/main/SettingsFragment.java
@@ -4,21 +4,92 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
+import com.google.firebase.auth.FirebaseUser;
 import com.kaimanden.betyou.R;
 import com.kaimanden.betyou.base.BaseFrg;
+import com.kaimanden.betyou.tools.AuthController;
+import com.kaimanden.betyou.tools.listeners.AuthListener;
 
 public class SettingsFragment extends BaseFrg {
+
+    private EditText edtName, edtPaypal, edtOldPass, edtPass, edtPassConfirm;
+    private Switch swNotifs;
+    private Button btnProfile, btnPass;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.frg_settings, container, false);
         initViews(v);
+        initButtons();
         return v;
     }
 
-    private void initViews(View v) {
 
+    private void initViews(View v) {
+        edtName = v.findViewById(R.id.frg_settings_name_edt);
+        edtPaypal = v.findViewById(R.id.frg_settings_paypal_edt);
+        edtOldPass = v.findViewById(R.id.frg_settings_oldpass_edt);
+        edtPass = v.findViewById(R.id.frg_settings_pass_edt);
+        edtPassConfirm = v.findViewById(R.id.frg_settings_pass_confirm_edt);
+        swNotifs = v.findViewById(R.id.frg_settings_notifications_sw);
+        btnProfile = v.findViewById(R.id.frg_settings_profile_btn);
+        btnPass = v.findViewById(R.id.frg_settings_pass_btn);
+    }
+
+    private void initButtons() {
+        btnPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changePassword();
+            }
+        });
+
+    }
+
+    private void changePassword() {
+        String oldPass = edtOldPass.getText().toString().trim();
+        String pass = edtPass.getText().toString().trim();
+        String passConfirm = edtPassConfirm.getText().toString().trim();
+        if (oldPass.isEmpty()){
+            String error = getString(R.string.error_empty_data);
+            edtOldPass.setError(error);
+            return;
+        }
+        if (pass.isEmpty()){
+            String error = getString(R.string.error_empty_data);
+            edtPass.setError(error);
+            return;
+        }
+        if (passConfirm.isEmpty()){
+            String error = getString(R.string.error_empty_data);
+            edtPassConfirm.setError(error);
+            return;
+        }
+        if (!passConfirm.equals(pass)){
+            String error = getString(R.string.error_pass_equals);
+            edtPassConfirm.setError(error);
+            return;
+        }
+
+        showLoading(true);
+        AuthController.init(getActivity()).changePass(oldPass, pass, new AuthListener() {
+            @Override
+            public void isOk(FirebaseUser user) {
+                String msg = getString(R.string.request_pass_changed);
+                showInfo(msg);
+                showLoading(false);
+            }
+
+            @Override
+            public void isKo(String error) {
+                showLoading(false);
+                showError(error);
+            }
+        });
     }
 }
--- app/src/main/java/com/kaimanden/betyou/tools/AuthController.java
+++ app/src/main/java/com/kaimanden/betyou/tools/AuthController.java
@@ -6,9 +6,12 @@ import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.kaimanden.betyou.R;
 import com.kaimanden.betyou.tools.listeners.AuthListener;
 
 public class AuthController {
@@ -37,30 +40,32 @@ public class AuthController {
         return (currentUser != null);
     }
 
+    private void checkTask(Task task, AuthListener listener){
+        if (task.isSuccessful()) {
+            FirebaseUser user = mAuth.getCurrentUser();
+            currentUser = user;
+            if (listener != null) {
+                listener.isOk(user);
+            }
+        } else {
+            currentUser = null;
+            // error
+            Exception ex = task.getException();
+            ex.printStackTrace();
+            String error = ex.getLocalizedMessage();
+            if (listener != null) {
+                listener.isKo(error);
+            }
+        }
+    }
+
     private OnCompleteListener<AuthResult> getCompletedTas(AuthListener listener){
 
         OnCompleteListener<AuthResult> task = new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    currentUser = user;
-                    if (listener != null) {
-                        listener.isOk(user);
-                    }
-                } else {
-                    currentUser = null;
-                    // error
-                    Exception ex = task.getException();
-                    ex.printStackTrace();
-                    String error = ex.getLocalizedMessage();
-                    if (listener != null) {
-                        listener.isKo(error);
-                    }
-                }
+                checkTask(task, listener);
             }
-
-            ;
         };
 
         return task;
@@ -88,5 +93,40 @@ public class AuthController {
             }
         };
         mAuth.sendPasswordResetEmail(email).addOnCompleteListener(act, task);
+    }
+
+    private void doChangePass(String pass, AuthListener listener){
+        OnCompleteListener<Void> task2 = new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                checkTask(task, listener);
+            }
+        };
+        currentUser.updatePassword(pass).addOnCompleteListener(task2);
+    }
+    public void changePass(String oldPass, String pass, AuthListener listener) {
+        if (!isLoged()){
+            String error = act.getString(R.string.error_not_loged);
+            listener.isKo(error);
+            return;
+        }
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        final String email = user.getEmail();
+        AuthCredential credential = EmailAuthProvider.getCredential(email, oldPass);
+        user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    doChangePass(pass, listener);
+                }else{
+                    String error = act.getString(R.string.error_not_valid_pass);
+                    listener.isKo(error);
+                }
+            }
+        });
+
+
+
     }
 }
