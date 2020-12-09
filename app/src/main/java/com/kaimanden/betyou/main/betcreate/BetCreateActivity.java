be55b54716e378699c1197c06990e7e5e2b7e7a9
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2020-12-09 21:31:48 +0100

    ADD: Bet => ADDIcons

--- app/src/main/java/com/kaimanden/betyou/main/betcreate/BetCreateActivity.java
+++ app/src/main/java/com/kaimanden/betyou/main/betcreate/BetCreateActivity.java
@@ -1,18 +1,65 @@
 package com.kaimanden.betyou.main.betcreate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.kaimanden.betyou.R;
+import com.kaimanden.betyou.base.BaseAct;
 
-public class BetCreateActivity extends AppCompatActivity {
+public class BetCreateActivity extends BaseAct {
 
     public static final String BETITEM = "bet item to create";
+
+    private ImageButton btnMinus, btnPlus;
+    private EditText edtMoney;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_bet_create);
+        initViews();
+        initButtons();
+    }
+
+    private void initViews() {
+        btnMinus = findViewById(R.id.act_bet_create_minus);
+        btnPlus  = findViewById(R.id.act_bet_create_plus);
+        edtMoney = findViewById(R.id.act_bet_create_money);
+    }
+
+    private void initButtons() {
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMoney(-1);
+            }
+        });
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeMoney(1);
+            }
+        });
+    }
+
+    private void changeMoney(int increase) {
+        String strMoney = edtMoney.getText().toString().trim();
+        Integer value = Integer.valueOf(strMoney);
+        value +=increase;
+
+        Integer minValue = getResources().getInteger(R.integer.min_value);
+        Integer maxValue = getResources().getInteger(R.integer.max_value);
+
+        if (value < minValue){
+            value = minValue;
+        }
+        if (value > maxValue){
+            value = maxValue;
+        }
+        edtMoney.setText(""+value);
+
     }
 }
