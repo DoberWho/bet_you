8223fe2f5e5eaf7ab233f2d557fa12e2cc379f9c
Jorge Parada Fernandez <jorge.parada@ctbarbanza.com>
2021-01-06 18:47:48 +0100

    FIX: Bet Data

--- app/src/main/java/com/kaimanden/betyou/main/HomeFragment.java
+++ app/src/main/java/com/kaimanden/betyou/main/HomeFragment.java
@@ -1,11 +1,15 @@
 package com.kaimanden.betyou.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.kaimanden.betyou.base.BaseFrg;
 import com.kaimanden.betyou.R;
 
@@ -21,5 +25,19 @@ public class HomeFragment extends BaseFrg {
 
     private void initViews(View v) {
 
+        FloatingActionButton btn = v.findViewById(R.id.frg_home_settings);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doAction();
+            }
+        });
+
+
+    }
+
+    private void doAction() {
+        Intent intent = new Intent(MediaStore.INTENT_ACTION_VIDEO_CAMERA);
+        startActivityForResult(intent, 0);
     }
 }
